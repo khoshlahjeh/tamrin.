39cb42d39cad8092fd5361754cc06a106104509d
khoshlahjeh <119805112+khoshlahjeh@users.noreply.github.com>
2023-01-14 21:58:12 +0330

    git-2
    this is git-2

---
+++ G2.py
@@ -0,0 +1,10 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> b=[]
+>>> c=int(input("enter your name :"))
+enter your name :12
+>>> while (c!=0):
+	a.append(c)
+	c=int(input("enter your name"))
+	print(a[::-1])
+	enter c num:5
